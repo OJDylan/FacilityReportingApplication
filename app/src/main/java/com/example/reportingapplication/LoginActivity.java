ed26474cbd1395ab51c2d1b2947c43edb4040858
Dylan <dooi2142@gmail.com>
2019-06-20 13:27:28 +0800

    Splash Screen Implemented, and other UI improvements

--- app/src/main/java/com/example/reportingapplication/LoginActivity.java
+++ app/src/main/java/com/example/reportingapplication/LoginActivity.java
@@ -1,13 +1,25 @@
 package com.example.reportingapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        Button button = findViewById(R.id.btn_login);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
