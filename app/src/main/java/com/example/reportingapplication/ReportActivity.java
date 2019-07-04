59ea28e06c19ffa1cbb5531efd8148bd14a7fd48
Dylan <dooi2142@gmail.com>
2019-07-04 17:10:33 +0800

    Implemented AutoCompleteTextView for City selection

--- app/src/main/java/com/example/reportingapplication/ReportActivity.java
+++ app/src/main/java/com/example/reportingapplication/ReportActivity.java
@@ -3,11 +3,20 @@ package com.example.reportingapplication;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class ReportActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+
+        String[] cities = getResources().getStringArray(R.array.cities);
+
+        AutoCompleteTextView editText = findViewById(R.id.city_autocomplete);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, cities);
+        editText.setAdapter(adapter);
     }
 }
